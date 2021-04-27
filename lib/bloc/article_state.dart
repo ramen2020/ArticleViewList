cf5e735d5dc31cb86f5268c9c6b6112e207f2f10
ramen2020 <vfvsfv.dlk22@gmail.com>
2021-04-28 01:40:05 +0900

    ✨：state追加

---
+++ lib/bloc/article_state.dart
@@ -0,0 +1,23 @@
+import 'package:qiita_client/models/article.dart';
+import 'package:meta/meta.dart';
+
+abstract class ArticleState {}
+
+// はじめ
+class ArticleUninitializedState extends ArticleState {}
+
+// tagをクリックした時
+class ArticleFetchingState extends ArticleState {}
+
+// 取得できた時
+class ArticleFetchedState extends ArticleState {
+  final List<Article> articles;
+
+  ArticleFetchedState({this.articles}) : assert(articles != null);
+}
+
+// エラー
+class ArticleErrorState extends ArticleState {}
+
+// いない
+class ArticleEmptyState extends ArticleState {}
