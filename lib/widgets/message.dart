ec0c5981842408e2ae2af26836afb31e94495ac0
ramen2020 <vfvsfv.dlk22@gmail.com>
2021-04-28 01:39:08 +0900

    ✨：メッセージwidget追加

---
+++ lib/widgets/message.dart
@@ -0,0 +1,21 @@
+import 'package:qiita_client/themes/themes.dart';
+import 'package:flutter/material.dart';
+
+class Message extends StatelessWidget {
+  final String message;
+
+  Message({this.message});
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Center(
+        child: Text(
+          message,
+          style: messageTextStyle,
+          textAlign: TextAlign.center,
+        ),
+      ),
+    );
+  }
+}
